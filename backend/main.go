0e9f3831a86abcc32fddf5d72d2dfc1bf0bf4481
Max Weis <weis98max@gmail.com>
2019-03-14 21:18:56 +0100

    started to send to rest api

--- backend/main.go
+++ backend/main.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
+
+//Values are the values send from the frontend
+type Values struct {
+	Name string
+	R    string
+	G    string
+	B    string
+}
 
 //Devices retrieves and sends all connected Devices
 func Devices(w http.ResponseWriter, r *http.Request) {
@@ -21,11 +30,31 @@ func rgb(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode("lmaoo")
 }
 
+func SendValues(w http.ResponseWriter, r *http.Request) {
+	val := Values{
+		Name: r.PostFormValue("name"),
+		R:    r.PostFormValue("r"),
+		G:    r.PostFormValue("g"),
+		B:    r.PostFormValue("b"),
+	}
+
+	log.Println(val)
+
+	answer := fmt.Sprintln(
+		"Successfully get values with name: ", val.Name,
+		" r: ", val.R,
+		" g: ", val.G,
+		" b: ", val.B)
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, answer)
+}
+
 // our main function
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/v1/CheckConnection", test).Methods("GET")
 	router.HandleFunc("/v1/GetDevices", Devices).Methods("GET")
-	router.HandleFunc("/v1/SendValues", rgb).Methods("POST")
+	router.HandleFunc("/v1/SendValues", SendValues).Methods("POST")
 	log.Fatal(http.ListenAndServe(":80", router))
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,49 @@
 $(document).ready(function () {
-    $('.rgb-slider').on('input', function () {
-        //console.log(this.value);
-        //console.log(this.id)
-        var r = $('#slider-r')[0].value
-        var g = $('#slider-g')[0].value
-        var b = $('#slider-b')[0].value
-        console.log(r, g, b);
+    //react to changes from the slider
+    // $('.rgb-slider').on('input', function () {
+    //     //var obj = {
+    //     //    name: $(".card-title").html(),
+    //     //    r: $('#slider-r')[0].value,
+    //     //    g: $('#slider-g')[0].value,
+    //     //    b: $('#slider-b')[0].value
+    //     //}
 
+    //     $("#name").val(
+    //         $(this)
+    //         .closest(".card-content")
+    //         .find(".card-title")[0].innerText
+    //     );
+    //     console.log($('#rgb-form').serialize());
+
+    //     var url = "http://rest/v1/sendValues/"
+
+    //     $.get("http://rest/v1/CheckConnection",
+    //         function (data) {
+    //             console.log(data);
+    //             alert("Can not connect to device")
+    //         },
+    //         "json"
+    //     );
+
+    //     $.ajax({
+    //         type: "POST",
+    //         url: url,
+    //         crossDomain: true,
+    //         data: $('#rgb-form').serialize(),
+    //         dataType: "text",
+    //         success: function (response) {
+    //             console.log(response);
+
+    //         }
+    //     });
+
+    // });
+
+    $(".switch").change(function (e) {
+        e.preventDefault();
+        if (!$(this).is(":checked")) {
+            console.log("ayy");
+
+        }
     });
 });
